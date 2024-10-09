af697092b0b3152b877021fd05a3d7e228284710
Ezequielfernandz <115805520+Ezequielfernandz@users.noreply.github.com>
2024-10-09 10:20:09 -0300

    Update registrodelospuntos.ts

--- registrodelospuntos.ts
+++ registrodelospuntos.ts
@@ -3,24 +3,24 @@ const equipos = {
     negro: { handball: 0, ajedrez: 0, resistencia: 0 }
 };
 
-// Función para agregar puntos
+
 function agregarPuntos(equipo) {
     const handball = parseInt(document.getElementById(`Handball-${equipo.charAt(0)}`).value) || 0;
     const ajedrez = parseInt(document.getElementById(`Ajedrez-${equipo.charAt(0)}`).value) || 0;
     const resistencia = parseInt(document.getElementById(`Resistencia-${equipo.charAt(0)}`).value) || 0;
 
-    // Actualizar puntos del equipo correspondiente
+    
     equipos[equipo].handball += handball;
     equipos[equipo].ajedrez += ajedrez;
     equipos[equipo].resistencia += resistencia;
 
-    // Limpiar los campos de entrada
+    
     document.getElementById(`Handball-${equipo.charAt(0)}`).value = '';
     document.getElementById(`Ajedrez-${equipo.charAt(0)}`).value = '';
     document.getElementById(`Resistencia-${equipo.charAt(0)}`).value = '';
 }
 
-// Función para mostrar los resultados
+
 function mostrarResultados() {
     const totalRojo = equipos.rojo.handball + equipos.rojo.ajedrez + equipos.rojo.resistencia;
     const totalNegro = equipos.negro.handball + equipos.negro.ajedrez + equipos.negro.resistencia;
