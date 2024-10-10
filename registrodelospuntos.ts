a107b610e7850ef59685c8ab7af74f0f82d6ba16
Ezequielfernandz <115805520+Ezequielfernandz@users.noreply.github.com>
2024-10-10 09:57:25 -0300

    Update registrodelospuntos.ts

--- registrodelospuntos.ts
+++ registrodelospuntos.ts
@@ -1,40 +1,15 @@
-const equipos = {
-    rojo: { handball: 0, ajedrez: 0, resistencia: 0 },
-    negro: { handball: 0, ajedrez: 0, resistencia: 0 }
+interface puntos{
+    punto: number;
+}
+
+let EquipoRojo: puntos = {
+        punto: 0
 };
 
+let EquipoNegro: puntos = {
+    punto: 0
+};
 
-function agregarPuntos(equipo) {
-    const handball = parseInt(document.getElementById(`Handball-${equipo.charAt(0)}`).value) || 0;
-    const ajedrez = parseInt(document.getElementById(`Ajedrez-${equipo.charAt(0)}`).value) || 0;
-    const resistencia = parseInt(document.getElementById(`Resistencia-${equipo.charAt(0)}`).value) || 0;
-
-    
-    equipos[equipo].handball += handball;
-    equipos[equipo].ajedrez += ajedrez;
-    equipos[equipo].resistencia += resistencia;
-
-    
-    document.getElementById(`Handball-${equipo.charAt(0)}`).value = '';
-    document.getElementById(`Ajedrez-${equipo.charAt(0)}`).value = '';
-    document.getElementById(`Resistencia-${equipo.charAt(0)}`).value = '';
-}
-
-
-function mostrarResultados() {
-    const totalRojo = equipos.rojo.handball + equipos.rojo.ajedrez + equipos.rojo.resistencia;
-    const totalNegro = equipos.negro.handball + equipos.negro.ajedrez + equipos.negro.resistencia;
-
-    let resultadoTexto = `Total Equipo Rojo: ${totalRojo} puntos<br>`;
-    resultadoTexto += `Total Equipo Negro: ${totalNegro} puntos<br>`;
-
-    if (totalRojo > totalNegro) {
-        resultadoTexto += 'El Equipo Rojo tiene más puntos.';
-    } else if (totalNegro > totalRojo) {
-        resultadoTexto += 'El Equipo Negro tiene más puntos.';
-    } else {
-        resultadoTexto += 'Ambos equipos tienen la misma cantidad de puntos.';
-    }
-
-    document.getElementById('resultado').innerHTML = resultadoTexto;
-}
+const HandballInputEquipoRojo - document.getElementById("Handball-EquipoRojo") as HTMLInputElement;
+const AjedresInputEquipoRojo - document.getElementById("Ajedres-EquipoRojo") as HTMLInputElement;
+const ResistenciaInputEquipoRojo - document.getElementById("Resistencia-EquipoRojo") as HTMLInputElement;
