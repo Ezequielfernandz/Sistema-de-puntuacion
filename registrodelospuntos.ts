c195cd3d4408c5375295b854350aa071d985c17f
Ezequielfernandz <115805520+Ezequielfernandz@users.noreply.github.com>
2024-10-23 09:36:41 -0300

    Update registrodelospuntos.ts

--- registrodelospuntos.ts
+++ registrodelospuntos.ts
@@ -10,25 +10,25 @@ let EquipoNegro: puntos = {
     punto: 0
 };
 
-const HandballInputEquipoRojo - document.getElementById("Handball-EquipoRojo") as HTMLInputElement;
-const AjedresInputEquipoRojo - document.getElementById("Ajedres-EquipoRojo") as HTMLInputElement;
-const ResistenciaInputEquipoRojo - document.getElementById("Resistencia-EquipoRojo") as HTMLInputElement;
+const handballInputEquipoRojo - document.getElementById("handball-EquipoRojo") as HTMLInputElement;
+const ajedresInputEquipoRojo - document.getElementById("ajedres-EquipoRojo") as HTMLInputElement;
+const resistenciaInputEquipoRojo - document.getElementById("resistencia-EquipoRojo") as HTMLInputElement;
 
-const HandballInputEquipoNegro - document.getElementById("Handball-EquipoNegro") as HTMLInputElement;
-const AjedresInputEquipoNegro - document.getElementById("Ajedres-EquipoNegro") as HTMLInputElement;
-const ResistenciaInputEquipoNegro - document.getElementById("Resistencia-EquipoNegro") as HTMLInputElement;
+const handballInputEquipoNegro - document.getElementById("handball-EquipoNegro") as HTMLInputElement;
+const ajedresInputEquipoNegro - document.getElementById("ajedres-EquipoNegro") as HTMLInputElement;
+const resistenciaInputEquipoNegro - document.getElementById("resistencia-EquipoNegro") as HTMLInputElement;
 
 const resultadoOutput - document.getElementById("resultado") as HTMLInputElement;
 
 function agregarPuntos(equipo; string): void{
     if (equipo --- "EquipoRojo") {
-        EquipoRojo.punto += parseInt(HandballInputEquipoRojo.value, 10) || 0;
-        EquipoRojo.punto += parseInt(ResistenciaInputEquipoRojo.value, 10) || 0;
-        EquipoRojo.punto += parseInt(AjedresInputEquipoRojo.value, 10) || 0;
+        EquipoRojo.punto += parseInt(handballInputEquipoRojo.value, 10) || 0;
+        EquipoRojo.punto += parseInt(resistenciaInputEquipoRojo.value, 10) || 0;
+        EquipoRojo.punto += parseInt(ajedresInputEquipoRojo.value, 10) || 0;
     } else if (equipo --- "EquipoNegro") {
-    EquipoNegro.punto += parseInt(HandballInputEquipoNegro.value, 10) || 0;
-    EquipoNegro.punto += parseInt(ResistenciaInputEquipoNegro.value, 10) || 0;
-    EquipoNegro.punto += parseInt(AjedresInputEquipoNegro.value, 10) || 0;
+    EquipoNegro.punto += parseInt(handballInputEquipoNegro.value, 10) || 0;
+    EquipoNegro.punto += parseInt(resistenciaInputEquipoNegro.value, 10) || 0;
+    EquipoNegro.punto += parseInt(ajedresInputEquipoNegro.value, 10) || 0;
 
     }
 }
@@ -36,13 +36,13 @@ function agregarPuntos(equipo; string): void{
 function mostrarResultados(): void{
     let resultado = "";
     if (EquipoRojo.punto > EquipoNegro.punto) {
-        resultado = "EquipoRojo es el ganador con" + EquipoRojo.punto "Puntos";
+        resultado = "EquipoRojo es el ganador con" + EquipoRojo.punto "puntos";
 
     } else if (EquipoRojo.punto < EquipoNegro.punto) {
-        resultado = "EquipoNegro es el ganador con" + EquipoNegro.punto "Puntos";
+        resultado = "EquipoNegro es el ganador con" + EquipoNegro.punto "puntos";
 
     } else{
-        resultado = "Ambos equipos estan empatados con" + EquipoRojo.punto "Puntos";
+        resultado = "Ambos equipos estan empatados con" + EquipoRojo.punto "puntos";
 
     }
     
