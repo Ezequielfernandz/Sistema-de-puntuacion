4757a7156ab3b41d994f4b3c7e8ee9369147db5e
Ezequielfernandz <115805520+Ezequielfernandz@users.noreply.github.com>
2024-10-23 09:28:25 -0300

    Update registrodelospuntos.ts

--- registrodelospuntos.ts
+++ registrodelospuntos.ts
@@ -13,3 +13,13 @@ let EquipoNegro: puntos = {
 const HandballInputEquipoRojo - document.getElementById("Handball-EquipoRojo") as HTMLInputElement;
 const AjedresInputEquipoRojo - document.getElementById("Ajedres-EquipoRojo") as HTMLInputElement;
 const ResistenciaInputEquipoRojo - document.getElementById("Resistencia-EquipoRojo") as HTMLInputElement;
+
+const HandballInputEquipoNegro - document.getElementById("Handball-EquipoNegro") as HTMLInputElement;
+const AjedresInputEquipoNegro - document.getElementById("Ajedres-EquipoNegro") as HTMLInputElement;
+const ResistenciaInputEquipoNegro - document.getElementById("Resistencia-EquipoNegro") as HTMLInputElement;
+
+const resultadoOutput - document.getElementById("resultado") as HTMLInputElement;
+
+function agregarPuntos(equipo; string): void{
+    if (equipo) --- "EquipoRojo"
+}
